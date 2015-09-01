77cb1629bf75313b6e4709808f23eb1d64ec90b4
Dylan Griffith <dyl.griffith@gmail.com>
2015-09-02 09:35:40 +1000

    WIP: Application + Supervisor tree

--- lib/smex.ex
+++ lib/smex.ex
@@ -1,2 +1,7 @@
 defmodule Smex do
+  use Application
+
+  def start(_type, _args) do
+    Smex.Supervisor.start_link
+  end
 end
---
+++ lib/smex/supervisor.ex
@@ -0,0 +1,15 @@
+defmodule Smex.Supervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  def init([]) do
+    children = [
+      worker(Smex.ACL, [])
+    ]
+
+    supervise(children, strategy: :one_for_one)
+  end
+end
