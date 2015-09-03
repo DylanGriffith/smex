0f22b7e8ff4367c69e2bff0b7b48cccde4243c2a
Dylan Griffith <dyl.griffith@gmail.com>
2015-09-03 20:59:31 +1000

    bidirectional ACL type -> hash string working

--- lib/smex/acl.ex
+++ lib/smex/acl.ex
@@ -1,8 +1,11 @@
 defmodule Smex.ACL do
-  use Protobuf, from: Path.expand(System.get_env("SMITH_ACL_PATH") || "../acls/*.proto")
+
+  use Protobuf, from: Path.expand(System.get_env("SMITH_ACL_PATH_GLOB") || Path.expand("test/acls/*.proto", __DIR__))
 
   def start_link do
-    Agent.start_link(fn -> HashDict.new end, name: :acl_cache)
+    pid = Agent.start_link(fn -> HashDict.new end, name: :acl_cache)
+    inverse_map_all_acls_to_hash
+    pid
   end
 
   @doc """
@@ -10,8 +13,8 @@ defmodule Smex.ACL do
 
   ## Examples
 
-    iex> Smex.ACL.acl_type_hash(ACL.Null)
-    "180sqw3"
+  iex> Smex.ACL.acl_type_hash(ACL.Null)
+  "180sqw3"
   """
   def acl_type_hash(type) do
     type = type
@@ -21,5 +24,26 @@ defmodule Smex.ACL do
     Murmur.hash(:x86_32, type)
     |> Integer.to_string(36)
     |> String.downcase
+  end
+
+  @doc """
+  Converts a hash string used by smex messaging to the corresponding ACL type.
+
+  ## Examples
+  iex> Smex.ACL.acl_type_from_hash("180sqw3")
+  ACL.Null
+  """
+  def acl_type_from_hash(hash) do
+    case Agent.get(:acl_cache, fn (dict) -> dict |> Dict.fetch(hash) end) do
+      {:ok, result} -> result
+      :error -> nil
+    end
+  end
+
+  defp inverse_map_all_acls_to_hash do
+    names = defs |> Enum.map(&(elem(elem(&1, 0), 1)))
+    names = names |> Enum.map(&({acl_type_hash(&1), &1}))
+
+    Agent.update(:acl_cache, fn(dict) -> (names |> Enum.into(dict)) end)
   end
 end
--- test/smex/acl_test.exs
+++ test/smex/acl_test.exs
@@ -3,11 +3,18 @@ defmodule Smex.ACL.Test do
   use ExUnit.Case
 
   test "ACLs are compiled properly" do
-    ACL.Dashboard.User.new
-    Smith.ACL.Tweets.new
+    ACL.Null.new
+    ACL.Notification.Update.new
+    ACL.Term.new
   end
 
   test "acl_type_hash returns the correct type hash" do
     assert Smex.ACL.acl_type_hash(ACL.Null) == "180sqw3"
+  end
+
+  test "I can map from a type hash to acl name" do
+    assert Smex.ACL.acl_type_from_hash("180sqw3") == ACL.Null
+    assert Smex.ACL.acl_type_from_hash("9qotwg") == ACL.Terms
+    assert Smex.ACL.acl_type_from_hash("bpk3uz") == ACL.Notification.Updates
   end
 end
