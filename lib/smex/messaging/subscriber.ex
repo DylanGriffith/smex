d0c61ddce48a53da35e9d1a731c90dbf87870c24
Dylan Griffith <dyl.griffith@gmail.com>
2015-09-12 12:41:12 +1000

    get Subscriber working

---
+++ lib/smex/messaging/subscriber.ex
@@ -0,0 +1,65 @@
+defmodule Smex.Messaging.Subscriber do
+  use GenServer
+
+  defstruct queue_name: nil, prefetch: 1, fanout: false, fanout_persistence: true, fanout_queue_suffix: nil, type: nil
+
+  defmacro __using__(opts) do
+
+    quote do
+      use GenServer
+
+      def init(_opts) do
+        opts = unquote(opts) |> Enum.into %{}
+        subscriber = Map.merge(%Smex.Messaging.Subscriber{}, opts)
+        channel = Smex.Messaging.channel
+        AMQP.Basic.qos(channel, prefetch_count: subscriber.prefetch)
+
+        exchange_name = "smith.#{subscriber.queue_name}"
+        queue_name = exchange_name # TODO: Support fanout
+        routing_key = exchange_name
+        error_queue = "#{queue_name}.error"
+
+        # Invalid messages will be sent here
+        AMQP.Queue.declare(channel, error_queue, durable: subscriber.fanout_persistence, auto_delete: !subscriber.fanout_persistence)
+
+        AMQP.Queue.declare(channel, queue_name, durable: subscriber.fanout_persistence, auto_delete: !subscriber.fanout_persistence)
+
+        # TODO: Support fanout
+        AMQP.Exchange.direct(channel, exchange_name, durable: true, auto_delete: false)
+
+        consumer_tag = AMQP.Basic.consume(channel, queue_name)
+
+        {:ok, state} = bootup
+        {:ok, %{inner_state: state, subscriber: subscriber, channel: channel}}
+      end
+
+      def handle_info({:basic_consume_ok, %{consumer_tag: consumer_tag}}, chan) do
+        {:noreply, chan}
+      end
+
+      # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
+      def handle_info({:basic_cancel, %{consumer_tag: consumer_tag}}, chan) do
+        {:stop, :normal, chan}
+      end
+
+      # Confirmation sent by the broker to the consumer process after a Basic.cancel
+      def handle_info({:basic_cancel_ok, %{consumer_tag: consumer_tag}}, chan) do
+        {:noreply, chan}
+      end
+
+      # Handle rabbitmq messages
+      def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered, type: type_hash}}, state) do
+        type = Smex.ACL.acl_type_from_hash(type_hash)
+        if type == state[:subscriber].type do
+          # TODO: Consider supporting updating the state somehow from the class using this module.
+          spawn fn -> consume(state[:inner_state], state[:channel], type.decode(payload), tag, redelivered) end
+        else
+          # TODO: Call handler function. Have default handler function implementation that sends to error queue.
+          raise "unsupported ACL type given with hash: #{type_hash} and type: #{type}"
+        end
+        {:noreply, state}
+      end
+
+    end
+  end
+end
--- test/smex_test.exs
+++ test/smex_test.exs
@@ -1,14 +1,41 @@
 defmodule SmexTest do
   use ExUnit.Case
 
-  test "#send sends to queue" do
-    :ok = Smex.publish("some_test_queue_foobar", ACL.Null.new(null: "helloworld"))
+  test "#publish sends to queue" do
+    message = ACL.Null.new(null: "helloworld")
+    assert Smex.publish("some_test_queue", message) == :ok
+    assert Smex.publish("some_test_queue", message) == :ok
+    assert Smex.publish("some_test_queue", message) == :ok
+    assert Smex.publish("some_test_queue", message) == :ok
+    assert Smex.publish("some_test_queue", message) == :ok
   end
 
-  test "#receive subscribes to queue" do
-    Smex.subscribe("some_test_queue", [type: ACL.Null], fn (payload, receiver) ->
-      IO.inspect(payload)
-      Smex.ack(receiver)
-    end)
+  test "#subscribe subscribes to queue" do
+    :ok = Smex.publish("some_test_queue", ACL.Null.new(null: "helloworld"))
+    :ok = Smex.publish("some_test_queue", ACL.Null.new(null: "helloworld"))
+    defmodule TestSubscriber do
+      use Smex.Messaging.Subscriber, queue_name: "some_test_queue", type: ACL.Null
+
+      def start_link do
+        GenServer.start_link(__MODULE__, [])
+      end
+
+      def bootup do
+        {:ok, %{mystate: true}}
+      end
+
+      def consume(_state, channel, payload, tag, _redelivered) do
+        assert payload.__struct__ == ACL.Null
+        Smex.ack(channel, tag)
+      end
+    end
+
+    # TODO: I don't think this test actually does anything apart from assert
+    # things compile. The only way I can think to get this to work is to
+    # somehow have this pid sent into the TestSubscriber on startup and then
+    # have the TestSubscriber `send` to that pid when it has actually consumed
+    # some messages then I can `receive` here and block until stuff actually
+    # happens.
+    TestSubscriber.start_link
   end
 end
