675b654d8dfec5047b06f3e2ccf6b614ba89b1b1
Dylan Griffith <dyl.griffith@gmail.com>
2015-09-12 14:55:47 +1000

    add TODO about supervision fixing

--- lib/smex/supervisor.ex
+++ lib/smex/supervisor.ex
@@ -6,6 +6,11 @@ defmodule Smex.Supervisor do
   end
 
   def init([]) do
+    # TODO: Fix supervision tree so that a restart to AMQP causes a restart to
+    # Smex.Messaging and a restart to Smex.Messaging triggers a restart to any
+    # Smex.Messaging.Publisher. Probably need Smex.Messaging.Publisher.start*
+    # to add it to supervision tree somehow. Or start_link could link itself
+    # against the already running Smex.Messaging.
     children = [
       worker(Smex.ACL, []),
       worker(Smex.Messaging, []),
